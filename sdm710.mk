06a306d7d125375534633ba57f51c80ad68a4c75
iamsaalim <saalimquadri1@gmail.com>
2020-09-13 09:50:12 +0530

    sdm710-common: Build Signapk

--- sdm710.mk
+++ sdm710.mk
@@ -359,6 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+    
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
