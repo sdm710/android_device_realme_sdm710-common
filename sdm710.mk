71d8681d1953ec61be6d48ec242ed9a85e24471c
MadhavSaladi <baaswanthmadhav@gmail.com>
2020-11-13 16:17:50 +0530

    sdm710-common: Drop lineage stuff
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -389,14 +389,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.realme_sdm710
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
