4112bbab71b11751d3571b59f129cdabe1248695
MadhavSaladi <baaswanthmadhav@gmail.com>
2020-11-13 21:02:20 +0530

    RMX1971: Drop RealmeParts

--- sdm710.mk
+++ sdm710.mk
@@ -208,10 +208,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# Doze
-PRODUCT_PACKAGES += \
-    RealmeParts
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710
