0a662ef858bfe76038fdc1ea9f1b5d41e77b57a9
improbir <ghosh_probir@yahoo.com>
2021-05-07 14:39:26 +0000

    sdm710-common: Build power hal 1.0
    * Add missing Power manifest (cannot find entry android.hardware.power@1.0::ipower/default in either framework or device manifest)
    * Fully fixed 2% drop issue
    
    Signed-off-by: improbir <ghosh_probir@yahoo.com>
    Signed-off-by: Sai Kishan <kishansai95@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -346,7 +346,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Protobuf
 PRODUCT_PACKAGES += \
