54b774ef4520651694200d2de7a3480433d4d19b
SagarMakhar <sagarmakhar@gmail.com>
2021-07-18 14:26:10 +0000

    sdm710-common: Add kernel tree to soong namespaces
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -403,7 +403,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    kernel/realme/sdm710
 
 # Telephony
 PRODUCT_PACKAGES += \
