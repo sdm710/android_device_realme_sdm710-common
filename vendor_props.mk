e554f513fa0b1e4344e4b0f93e697719d3f5a235
Sai Kishan <kishansai95@gmail.com>
2021-08-17 04:02:21 +0000

    sdm710-common: Disable blurs
    *Also expensive
    
    Signed-off-by: Sai Kishan <kishansai95@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -104,6 +104,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
     persist.bluetooth.bluetooth_audio_hal.disabled=true
 
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.gyro.disable=0
